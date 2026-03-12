b73ba2c7590fb4141eaad210eb622455f248c060
Ali Raza Marchal <kh.marchal@gmail.com>
2026-03-12 20:48:51 +0500

    feat: replace AppLogoIcon with img element for BAJK Icon in AppHeader and AppLogo components

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,12 @@
-import AppLogoIcon from '@/components/app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
-                    <div className="flex h-9 w-9 items-center justify-center">
-                        <AppLogoIcon className="size-9 fill-current text-black dark:text-white" />
+            <div className="flex h-9 w-9 items-center justify-center">
+                <img
+                    src="/favicon-32x32.png"
+                    alt="BAJK Icon"
+                    className="h-8 w-8 object-contain"
+                />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
