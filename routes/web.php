c18fcdd28024b830e844803000513119a3b897c4
Ali Raza Marchal <kh.marchal@gmail.com>
2026-03-18 17:38:10 +0500

    feat: update home route to redirect to login and adjust related test

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use App\Http\Controllers\NadraVerifications\NadraVerificationController;
 use App\Http\Controllers\Users\UserController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::redirect('/', '/login')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
